af44addb3cb40ae195079f9c1354144b9d76d887
maksimmer348@gmail.com <123456mb>
2021-04-06 17:51:44 +0300

    аоаоа

--- ExperementNetCore/Program.cs
+++ ExperementNetCore/Program.cs
@@ -1,104 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Channels;
-using PersonInf;
 
 namespace ExperementNetCore
 {
-    delegate void AccountHandler(AccounArgs message);
-
-    class AccounArgs
-    {
-        public string Name;
-        public AccounArgs(string name)
-        {
-            Name = name;
-        }
-    }
-
-    class CompanyArg: AccounArgs
-    {
-        public string Company;
-        public CompanyArg(string name, string company)
-            : base(name)
-        {
-            Company = company;
-        }
-    }
-
-    class Person
-    {
-
-        public Person(string name, AccountHandler k)
-        {
-            _notify += k;
-            Name = name;
-        }
-
-
-        public event AccountHandler _notify;
-        private string name;
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                if (value != name)
-                {
-                    name = value;
-                    DisplayTo();
-                }
-                
-            }
-        }
-
-        public virtual void DisplayTo()
-        {
-            _notify?.Invoke(new AccounArgs(name));
-        }
-
-    }
-    class Employee : Person
-    {
-        public string Company { get; set; }
-
-        public Employee(string name, string company, AccountHandler k)
-            : base(name, k)
-        {
-            Company = company;
-        }
-
-        public override void DisplayTo()
-        {
-
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Person p = new Person("Max", Display);
-            
-            p = new Employee("Mixa","KEK", Display);
-            
-            Employee e = new Employee("Lexa","LLOOOL", Display);
-            
-            Console.Read();
-        }
+          
+            Person p1 = new Person {Name = "Bill", Age = 34};
+            Person p2 = new Person {Name = "Tom", Age = 23};
 
-        static void Display(string name)
-        {
-            Console.WriteLine(name);
+            Person p3 = new Person {Name = "Alice", Age = 21};
+            Person p4 = new Person {Name = "Maximmer", Age = 21};
+            Person[] people = new Person[] {p1, p2, p4, p3};
+
+
+            Person[] peopleLenght = new Person[] { p1, p2, p4, p3 };
+            Array.Sort(peopleLenght, new PeopleComparer());
+
+            foreach (Person p in peopleLenght)
+            {
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            }
         }
-        static void Display(string name, string company)
+    }
+
+   
+
+    class Person : IComparable<Person> 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person o)
         {
-            Console.WriteLine($"{name} {company}");
+            return this.Age.CompareTo(o.Age); 
+        }
+    }
+
+    class PeopleComparer : IComparer<Person>//В данном случае используется обобщенная версия интерфейса IComparer, чтобы не делать излишних преобразований типов.
+    {
+        public int Compare(Person p1, Person p2)//Метод Compare предназначен для сравнения двух объектов o1 и o2. Он также возвращает три значения,
+            //в зависимости от результата сравнения: если первый объект больше второго, то возвращается число больше 0, если меньше
+            //- то число меньше нуля; если оба объекта равны, возвращается ноль.
+        {
+            if (p1.Name.Length > p2.Name.Length)//он сравнивает объекты в зависимости от длины строки - значения свойства Name 
+                return 1;
+            else if (p1.Name.Length < p2.Name.Length)
+                return -1;
+            else
+                return 0;
         }
     }
 }
